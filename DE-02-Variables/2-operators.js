3c3c5efe71e10e29aeea2c480d9f182a5e96e2cd
Sinan <sinankzu@gmail.com>
2022-09-07 10:27:41 +0200

    bi commitleyelim ne kyederiz

--- DE-02-Variables/2-operators.js
+++ DE-02-Variables/2-operators.js
@@ -70,3 +70,105 @@ console.log(e);
 
 c*=10;
 console.log(c);
+
+
+//*ÇARPMA VE ÜS ALMA
+//********************** */
+
+const pi=3.14;
+const r=5;
+const alan=pi*r**2;//? üs alma (**) ile olur
+const cevre=Math.ceil(2*pi*r);
+console.log(alan, cevre);
+
+//?Bazı fonksiyonlar
+// Math.floor();  //* her zaman en yakın alt tamsayıya yuvarlar
+// Math.ceil();  //* her zaman en yakın üst tam sayıya yuvarlar
+// Math.trunc(); //* sayının tam kısmını alır.
+// Math.round(); //* en yakın tam sayıya yuvarlar.
+
+// Math.random(); //* 0 ve 1 arasında rasgele sayı üretir.
+
+
+//*-----MOD ALMA-----*********
+
+const number=456;
+const birler=number%10;
+const onlar=Math.floor(number/10)%10;
+const yüzler=Math.trunc(number/100);
+console.log(birler,onlar,yüzler);
+
+// * =======================================================
+// *                 KARŞILAŞTIRMA OPERATÖRLERİ
+// * =======================================================
+
+const sayi1=4;
+console.log(sayi1==4);//true
+console.log(sayi1=="4");//true
+console.log(sayi1==="4");//false
+
+console.log(sayi1!=4);//false
+console.log(sayi1!="4");//false
+
+console.log(sayi1>4);//false
+console.log(sayi1>=4);//true
+
+
+// * =======================================================
+// *                 MANTIKSAL OPERATÖRLER
+// * =======================================================
+
+//? TRUE
+console.log(Boolean(5));//true
+console.log(Boolean(-5));//true
+console.log(Boolean(-15.5));//true
+
+//? 5 falsy
+console.log(Boolean(0)); //false
+console.log(Boolean(null));//false
+console.log(Boolean(""));//false
+console.log(Boolean(undefined));//false
+console.log(Boolean(NaN));//false
+
+const v1=false || 0 || 12.6 || true || false || null
+console.log(v1);
+
+const v2= false || 0 || null || undefined || NaN;
+console.log(v2);
+
+const v3= 5 && true && true && 0 && "";
+console.log(v3);
+
+const v4= 6 && true && 12.5 && 7;
+console.log(v4);//7
+
+//*DEĞİL !
+
+console.log(!v4);//false
+console.log(!v2);//true
+
+
+// * =======================================================
+// *                TİP DÖNÜŞÜMLERİ
+// * =======================================================
+
+const para= Number("1000") +Number("900")
+
+console.log(para);
+
+
+
+console.log(Number("")); //0
+console.log(Number(null)); //0
+console.log(Number(true));//1
+
+
+//* -------- NULLISH COALESCING OPERATOR----------- */
+//?  Bir ifadenin null veya undefined olması durumuna göre seçim yapmamıza olanak sağlayan bir operatördür.eğer kullanıcı input a null girer yada hiçbişey (undefined) girmezse, belirteceğim 2. ifadeyi (noname) kabul et
+
+
+const isim1 = null; // ?null
+const defaultIsim = "noname";
+;
+// isim1 true ise onu döndür yoksa 2. ifadeyi
+console.log(isim1 ?? defaultIsim); //noname 
