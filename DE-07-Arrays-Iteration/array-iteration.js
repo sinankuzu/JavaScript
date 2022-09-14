619aab4f2f25677b037dc1c7ff634978798d6499
Sinan <sinankzu@gmail.com>
2022-09-14 23:50:55 +0200

    guncel

--- DE-07-Arrays-Iteration/array-iteration.js
+++ DE-07-Arrays-Iteration/array-iteration.js
@@ -1,8 +1,92 @@
-const prices = [100,250,50,90]
-prices.forEach((fiyat)=>console.log(fiyat));
+//* ======================================================
+//*                   FOREACH METHOD
+//! NOT: forEach metodu void bir metottur. (Yani herhangi bir değer döndürmez.log(forEach..)=undefined) terminal işlemdir, diziyi fonksiyon içinde yazdırır ve tüketir bitirir.
 
-let toplam = 0;
-prices.forEach((p)=>console.log(toplam +=p))
-const names=["hilal", "muhammed", "guler", "sinan"]
+//*======================================================
 
-const yeniIsim names.map ((isim)=>isim.toUpperCase();
+//*----------ORNEK-----------
+
+const prices=[100, 250, 50, 90];
+
+prices.forEach((fiyat)=>console.log(fiyat))
+
+let toplam=0;
+let gitti=  prices.forEach((p)=>console.log(toplam+=p));
+
+console.log(gitti);//undefined
+
+//*********************************ORNEK******* */
+
+
+//* p => prices dizisinin her bir elemanı
+//* i => indis
+//* prices=> prices dizisidir.
+// const prices=[100, 250, 50, 90];
+let toplam1=0;
+
+prices.forEach((p,i, prices)=>{
+
+toplam1+=p
+console.log(`${i}. toplam: ${toplam1}`);
+
+prices[i]=p+10;
+//*bu şekilde diziyi kalıcı değiştiriyoruz
+
+})
+
+console.log(prices);
+
+
+//* ======================================================
+//*                       MAP METHOD
+//!map yapılan değişikliği bir diziye atıp ve bunu döndürebilir, tüketmez foreach gibi.
+//* ======================================================
+
+//*-------------- ÖRNEK -------------------
+//* names dizisinin içindeki her ismi büyük harf olarak
+//* yeni bir diziye saklayalım.
+
+const names=["hilal","muhammed","güler","faruk","dogukan", "mevlüt", "sinan"]
+
+const yeniIsim =  names.map((isim)=>isim.toUpperCase());
+
+ console.log(yeniIsim);
+console.log(names);
+
+
+//*örnek
+//* fiyatlar dizisindeki ürün fiyatlarin 250 TL altında olanlarına
+//* %50 zam, diğerlerine de %20 zam yapılmak isteniyor. Ayrıca
+//* zamli fiyatlar kalıcı olarak diziye işlenmek isteniyor fiyatlar[1]=eski fiyat*1.5 gibi
+//* 1. ürünün zamlı fiyati 110 TL gibi
+//* p => prices dizisinin her bir elemanı
+//* i => indis
+//* array=> fiyatlar dizisidir.
+
+const fiyatlar = [100, 250, 50, 90];
+
+fiyatlar.map((p,i,array)=>{
+
+if(p<250){
+ array[i] =  p*1.5
+
+}else
+array[i]=p*1.2
+})
+console.log(fiyatlar);
+
+
+//* ======================================================
+//*                       FILTER METHOD
+//* ======================================================
+//* prices array'inde fiyatı 100 TL den az olanlari ayri
+//* bir diziye saklayalim.
+
+ const yeniDizi= prices.filter((p)=> p<100)
+
+console.log(yeniDizi);
+
+
+ 
+
+
--- DE-07-Arrays-Iteration/array-loops.js
+++ DE-07-Arrays-Iteration/array-loops.js
@@ -5,22 +5,27 @@
 //* ======================================================
 //*                     FOR LOOP
 //* ======================================================
+//*ÖRNEK: dizi1 arrayindeki pozitif sayılarla negatif sayıları 2 ayrı array de toplayınız
+const diziler1=[-5, 15, 22, -4, 45, 78 ]
+const negatifler1=[];
+const pozitifler1=[];
 
+const dizilereAyir1=(dizi)=>{
 
-const dizi1 = [-5, 15, 22, -4, 45, 78];
-const negatifler1 = [];
-const pozitifler1 = [];
+for(let i=0; i<dizi.length; i++){
 
-const dizilereAyir1 = (dizi) => {
-  for (let i = 0; i < dizi.length; i++) {
-    if (dizi1[i] < 0) negatifler1.push(dizi[i]);
-    else pozitifler1.push(dizi[i]);
-  }
-};
-dizilereAyir1(dizi1);
+ if (dizi[i]<0) negatifler1.push(dizi[i])
 
+else pozitifler1.push(dizi[i])
+
+}
 console.log(pozitifler1);
 console.log(negatifler1);
+}
+dizilereAyir1(diziler1)
+
+// console.log(pozitifler1);
+// console.log(negatifler1);
 
 //***FOR IN */
 
@@ -39,6 +44,25 @@ const dizilereAyir2 = (osman) => {
 dizilereAyir2(diziler2);
 
 
+
+//****FOR OF */
+
+
+const diziler3 = [-5, 15, 22, -4, 45, 78];
+const negatifler3 = [];
+const pozitifler3 = [];
+
+const dizilereAyir3 = (osman) => {
+  for (let sayı  of osman) {
+    if (sayı < 0) negatifler3.push(sayı);
+    else pozitifler3.push(sayı);
+  }
+  console.log(pozitifler3);
+  console.log(negatifler3);
+};
+dizilereAyir3(diziler3);
+
+
 //* ======================================================
 //*                   FOR IN LOOP
 //* ======================================================
@@ -47,35 +71,56 @@ dizilereAyir2(diziler2);
 //* SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
 //* bir dizide saklandığını varsayalım.Bu hayvanları türüne
 //* göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
-//*  ana programa döndürecek fonksiyonu yazınız. Eğer o
-//* türden bir hayvan yok ise bulunamadığını yazdırsın.
+//*  ana programa döndürecek fonksiyonu yazınız. 
 //*--------------------------------------------------------
 
-const animal = [
-  "fil",
-  "aslan",
-  "deve",
-  "fil",
-  "kaplan",
-  "fil",
-  "deve",
-  "aslan",
-  "aslan",
-  "aslan",
-];
+// const animal=["fil", "aslan", "deve","fil", "kaplan","fil","deve", "aslan", "aslan", "aslan"]
+//  // const userAnimal=prompt("lütfen bir animal ismi giriniz")
 
-const userAnimal=prompt("lutfen bir hayvan ismi giriniz");
+// const findAnimal=(animal, userAnimal)=>{
 
-const findAnimal=(animal, userAnimal)=>{
+// let sayac = 0;
 
-    let sayac = 0;
+// for(let i in animal){
 
-    for(let i in animal){
-        if(animal[i] == userAnimal)
-        sayac++
-    }
+// if(animal[i]==userAnimal)
+
+// sayac++
+// }
+// return `${userAnimal} hayvanından ${sayac} adet vardır`
+
+// }
+// alert( findAnimal(animal, userAnimal ))
+//! global de tanımlanan değişkenleri fonksiyon içinde kullanacaksam, fonksiyona parametreyle göndermem zorunlu değildir, zaten süslü içinden globaldekilere erişebilirim, parametre ile göndereceksem de, fonksiyon içinde onlara farklı isim takabilirim yani animal dizisini gönderip a diye karşılayıp, fonksiyon içinde a ismiyle kullanabilirim
+
+//* ======================================================
+//*                   FOR OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+
+//* ORNEK: alttaki dizinin elemanlarını yanyana string olarak yazdır (for of döngüsü kullanın)
+
+const arabalar=["bmw","mercedes","audi", "volvo"]
+
+let arabalarString="";
+
+for(let i of arabalar) 
+ arabalarString = arabalarString + i + " "
+
+console.log(arabalarString);
+
+//!araba, arabalar dizisinin herbir elemanı
+//? 2.yol
+// console.log(arabalar.join(" "));
+
+//! for loop=> Bir dizi üzerinde yineleme yapmanın orijinal yollarından biridir.	
+//? forEach=> Bir dizi üzerinde yineleme yapmak için daha az kod içeren daha yeni bir yoldur.
+//! for loop=>Performansta daha hızlıdır.	
+//? forEach=> Performansta geleneksel döngüden daha yavaştır.
+//! for loop=> Break ifadesi döngüden çıkmak için kullanılabilir.	
+//? forEach=> Break ifadesi, geri arama işlevi nedeniyle kullanılamaz.
 
 
-}
 
-findAnimal(animal, userAnimal)
+
