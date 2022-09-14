3bf45d5157acbe11f4cd77dd6096be99759ba29a
Sinan <sinankzu@gmail.com>
2022-09-14 21:33:38 +0200

    go

---
+++ DE-07-Arrays-Iteration/array-iteration.js
@@ -0,0 +1,8 @@
+const prices = [100,250,50,90]
+prices.forEach((fiyat)=>console.log(fiyat));
+
+let toplam = 0;
+prices.forEach((p)=>console.log(toplam +=p))
+const names=["hilal", "muhammed", "guler", "sinan"]
+
+const yeniIsim names.map ((isim)=>isim.toUpperCase();
