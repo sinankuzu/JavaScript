cd268bc048548a0d826c7fe40cb25daef909e463
Sinan <sinankzu@gmail.com>
2022-09-14 09:53:46 +0200

    go go

---
+++ DE-07-Arrays-Iteration/array-loops.js
@@ -0,0 +1,65 @@
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//*                       ITERATION IN ARRAYS
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//* ======================================================
+//*                     FOR LOOP
+//* ======================================================
+
+
+const dizi1 = [-5, 15, 22, -4, 45, 78];
+const negatifler1 = [];
+const pozitifler1 = [];
+
+const dizilereAyir1 = (dizi) => {
+  for (let i = 0; i < dizi.length; i++) {
+    if (dizi1[i] < 0) negatifler1.push(dizi[i]);
+    else pozitifler1.push(dizi[i]);
+  }
+};
+dizilereAyir1(dizi1);
+
+console.log(pozitifler1);
+console.log(negatifler1);
+
+//***FOR IN */
+
+const diziler2 = [-5, 15, 22, -4, 45, 78];
+const negatifler2 = [];
+const pozitifler2 = [];
+
+const dizilereAyir2 = (osman) => {
+  for (let i  in osman) {
+    if (osman[i] < 0) negatifler2.push(osman[i]);
+    else pozitifler2.push(osman[i]);
+  }
+  console.log(pozitifler2);
+  console.log(negatifler2);
+};
+dizilereAyir2(diziler2);
+
+
+//* ======================================================
+//*                   FOR IN LOOP
+//* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+//* bir dizide saklandığını varsayalım.Bu hayvanları türüne
+//* göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//*  ana programa döndürecek fonksiyonu yazınız. Eğer o
+//* türden bir hayvan yok ise bulunamadığını yazdırsın.
+//*--------------------------------------------------------
+
+const animal = [
+  "fil",
+  "aslan",
+  "deve",
+  "fil",
+  "kaplan",
+  "fil",
+  "deve",
+  "aslan",
+  "aslan",
+  "aslan",
+];
