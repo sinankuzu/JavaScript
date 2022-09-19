0d3915435417d22773208298412b6d64a90f1a4b
Sinan <sinankzu@gmail.com>
2022-09-19 12:26:47 +0200

    aferin

---
+++ DE-10-DOM-Intro/example-1/calculate.js
@@ -0,0 +1,10 @@
+const yaricapInput = document.querySelector("#yaricap")
+
+const buton =document.querySelector("#btn")
+
+buton.onclick=()=>{
+
+const r = yaricapInput.value;
+const alan = Math.PI * r ** 2;
+
+}
