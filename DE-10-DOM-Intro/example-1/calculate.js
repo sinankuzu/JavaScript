db0f9dd6a21d08866c7ec54e100dfd011a05e48a
Sinan <sinankzu@gmail.com>
2022-09-19 12:34:43 +0200

    ll

--- DE-10-DOM-Intro/example-1/calculate.js
+++ DE-10-DOM-Intro/example-1/calculate.js
@@ -5,6 +5,10 @@ const buton =document.querySelector("#btn")
 buton.onclick=()=>{
 
 const r = yaricapInput.value;
-const alan = Math.PI * r ** 2;
+const alan = (Math.PI * r ** 2).toFixed(2);
+const cevre = (2*Math.PI * r).toFixed(2);
 
+document.querySelector(".alan").textContent = alan
+document.querySelector(".cevre").textContent = cevre;
 }
+
