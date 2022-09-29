8c1411a114d99477391f09c0714b09e9fcf6dde6
Sinan <sinankzu@gmail.com>
2022-09-29 09:41:22 +0200

    yapistir

--- DE-13-BILMEM/app.js
+++ DE-13-BILMEM/app.js
@@ -7,3 +7,61 @@ let sepettekiler = [
   { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
   { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
 ];
+
+
+sepettekiler.forEach((urun)=>{
+
+document.querySelector(
+  "#urun-rowlari"
+).innerHTML += `<div class="card mb-3" style="max-width: 540px;">
+
+  <div class="row g-0">
+
+    <div class="col-md-5">
+      <img src=    class="img-fluid rounded-start" alt="...">
+    </div>
+
+    <div class="col-md-7">
+
+      <div class="card-body">
+      
+        <h5 class="card-title">      </h5>
+        
+             <div class="ürün-price">
+                    <p class="text-warning h2">$
+                      <span class="indirim-price">   </span>
+                      <span class="h5 text-dark text-decoration-line-through"> </span>
+                    </p>
+                  </div>
+
+                  
+                  <div
+                    class="border border-1 border-dark shadow-lg d-flex justify-content-center p-2"
+                  >
+                    <div class="adet-controller">
+                      <button class="btn btn-secondary btn-sm">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                      <p class="d-inline mx-4" id="ürün-adet"></p>
+                      <button class="btn btn-secondary btn-sm">
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+
+                  </div>
+
+                  <div class="ürün-removal mt-4">
+                    <button class="btn btn-danger btn-sm w-100 remove-ürün">
+                      <i class="fa-solid fa-trash-can me-2"></i>Remove
+                    </button>
+                  </div>
+
+                  <div class="mt-2">
+                    Ürün Toplam: $<span class="ürün-toplam"></span>
+                  </div>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+})
