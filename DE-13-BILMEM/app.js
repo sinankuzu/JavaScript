a5f7d4dc57995119b2f8b050408bf0522707ca9f
Sinan <sinankzu@gmail.com>
2022-09-29 09:38:09 +0200

    ee

---
+++ DE-13-BILMEM/app.js
@@ -0,0 +1,9 @@
+//* ======================================================================
+//*                 Checkout Page Solution
+//*map filter, dest,spread=======================================================================
+
+let sepettekiler = [
+  { name: "Vintage Backpack", price: 34.99, adet: 1, img: "./img/photo1.png" },
+  { name: "Levi Shoes", price: 40.99, adet: 1, img: "./img/photo2.png" },
+  { name: "Antique Clock", price: 69.99, adet: 1, img: "./img/photo3.jpg" },
+];
