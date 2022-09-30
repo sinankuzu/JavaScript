7f7630a836642a9af79e9564feef2e86785d423d
Sinan <sinankzu@gmail.com>
2022-09-30 11:40:34 +0200

    fetchlendi

--- DE-16-Async/2-promise.js
+++ DE-16-Async/2-promise.js
@@ -46,3 +46,55 @@ promise1.then((variable1) =>{
 }).catch((error)=>{
     console.log(error);
 })
+
+//* Example-2: TEA TIME
+//* -----------------------------------------------------------------------
+//* 1. su kaynayana kadar bekleyin -> kettle -> work or failed
+//* 2. çay ekle --> çay var ya da yok
+//* 3. demlenmesini bekle
+// const bekle = (ms) => {
+//   const start = new Date().getTime();
+//   while (new Date().getTime() < start + ms);
+// };
+//!ilk then de sıkıntı varsa direk catch e düşer
+const brewTea = () => {
+  boil()
+    .then((status1) => {
+      console.log(status1);
+      return addTea();
+    })
+    .then((status2) => {
+      console.log(status2);
+      // bekle(1000);
+      return 'Tea is ready. Bon Appetite';
+    })
+    .then((status3) => {
+      console.log(status3);
+    })
+    .catch((err) => console.log(err));
+};
+const boil = () => {
+  return new Promise((resolve, reject) => {
+    const boiled = Math.floor(Math.random() * 5);//!0 tutarsa false
+    if (boiled) {
+      // bekle(1000);
+      resolve('Water Boiled');
+    } else {
+      reject(new Error('Kettle Failed'));
+    }
+  });
+};
+
+const addTea = () => {
+  return new Promise((resolve, reject) => {
+    const teaAvalaible = Math.floor(Math.random() * 3);
+    if (teaAvalaible) {
+      // bekle(1000);
+      resolve('Tea was added');
+    } else {
+      reject(new Error('Tea unavailable'));
+    }
+  });
+};
+
+brewTea();
