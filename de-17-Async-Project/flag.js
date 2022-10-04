21d07b1de88ee105131630cf18f0ec963ffffa66
Sinan <sinankzu@gmail.com>
2022-10-04 11:52:35 +0200

    edelim

--- de-17-Async-Project/flag.js
+++ de-17-Async-Project/flag.js
@@ -1,4 +1,35 @@
-const getirCountry = () =>{
+const getirCountry =async (name) =>{
+try {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+    if (!res.ok) {
+      throw new Error("url de hata var");
+    }
+    const data = await res.json();
+    // console.log(data);
+    // console.log(data[0]);
+    ekranaBastir(data[0])
+} catch (error) {
+    console.log(error);
+}
 
-    fetch(`https://restcountries.com/v3.1/name/${name}`);
+
+}
+let dizi = ["turkey", "peru", "canada"]
+getirCountry("turkey")
+
+ekranaBastir = (country) =>{
+    document.querySelector(
+      ".countries"
+    ).innerHTML = `<div class="card text-start w-50 shadow-lg bg-light">
+      <img src=${country.flags.png} class="card-img-top" alt="...">
+      <div class="card-body">
+         <h5 class="card-title">${country.name.common}</h5>
+         <p class="card-text"></p>
+      </div>
+      <ul class="list-group list-group-flush">
+         <li class="list-group-item"><i class="fas fa-lg fa-landmark"></i> ${country.capital} </li>
+         <li class="list-group-item"><i class="fas fa-lg fa-comments"></i> ${country.languages.tur}</li>
+         <li class="list-group-item"><i class="fas fa-lg fa-money-bill-wave"> ${country.currencies.TRY.name}</i> </li>
+      </ul>
+   </div> `;    
 }
